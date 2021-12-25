137851f118dcb547dfc6b1fd37c905c989998808
Dogey11 <69096657+Dogey11@users.noreply.github.com>
2021-12-24 22:04:16 -0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-let hellow = "Hello World!";
-function yeet() {
-    console.log(hellow);
-    document.getElementById("helloText").textContent = "yeet";
+let HelloW = "Hello World!";
+
+function yeet(hello) {
+    if (hello) {
+        document.getElementById("helloText").textContent = HelloW;
+        console.log(HelloW);
+    }
+    else if (!hello) {
+        document.getElementById("helloText").textContent = "yeet";
+        console.log(HelloW);
+    }
 }
-document.getElementById("clickMe").onclick = yeet;
+
+document.getElementById("clickMe").onclick = function() {
+    yeet(true);
+}
+
+document.getElementById("fancypants").onclick = function() {
+    yeet(false);
+}
