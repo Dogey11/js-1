f8169f6d51137dde6288a0026cc0b9678b1c8a63
Dogey11 <69096657+Dogey11@users.noreply.github.com>
2021-12-25 21:02:41 -0800

    rickroll

--- index.js
+++ index.js
@@ -11,10 +11,22 @@ function yeet(hello) {
     }
 }
 
+function textbox() {
+    document.getElementById("joe").innerHTML = document.getElementById("textbox").value;
+}
+
 document.getElementById("clickMe").onclick = function() {
     yeet(true);
 }
 
 document.getElementById("fancypants").onclick = function() {
     yeet(false);
+}
+
+document.getElementById("rickroll").onclick = function() {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+
+document.getElementById("textbox").onkeyup = function() {
+    textbox();
 }
